e832e286ddda5408c7cf32adae6836f2ca9b4ee5
Ilya Zelnikov <asv67@seznam.cz>
2023-02-27 19:30:12 +0100

    fixed bugs

--- src/diffTree.js
+++ src/diffTree.js
@@ -1,12 +1,11 @@
 import _ from 'lodash';
 
-const getJoinKey = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));  
+const getJoinKey = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
 
 const getDiffTree = (obj1, obj2) => {
   const keys = getJoinKey(obj1, obj2);
   const diffTree = keys.map((key) => {
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      
       return ['nested', [key, getDiffTree(obj1[key], obj2[key])]];
     } if (obj1[key] === obj2[key]) {
       return ['unchanged', [key, obj1[key]]];
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import formatResult from './formatters/index.js';
 
 const genDiff = (file1, file2, style = 'stylish') => {
   const styleAnswer = (_.isObject(style)) ? style.format : style;
-  
+
   const formatFile1 = path.extname(file1).slice(1);
   const pathFile1 = path.resolve(file1);
   const contentFile1 = fs.readFileSync(pathFile1, 'utf8');
@@ -17,7 +17,7 @@ const genDiff = (file1, file2, style = 'stylish') => {
 
   const obj1 = parse(formatFile1, contentFile1);
   const obj2 = parse(formatFile2, contentFile2);
-  
+
   const diffTree = getDiffTree(obj1, obj2);
   return formatResult(diffTree, styleAnswer);
 };
