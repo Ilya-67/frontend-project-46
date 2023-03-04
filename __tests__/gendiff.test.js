d29d97fa99501e7e76b74f6cd6983a4f75010375
Ilya Zelnikov <asv67@seznam.cz>
2023-03-04 18:29:16 +0100

    fixed bugs

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,20 +9,22 @@ const getFilePath = (referenceFileName) => path.join(dirname1, '..', '__fixtures
 
 describe.each([
   {
-    a: 'file1.json', b: 'file2.yml', style: undefined, expected: 'result_stylish.txt'
-  }, 
-  { 
-    a: 'file1.json', b: 'file3.yaml', style: 'stylish', expected: 'result_with_empty_file.txt'
+    a: 'file1.json', b: 'file2.yml', style: undefined, expected: 'result_stylish.txt',
   },
   {
-    a: 'file1.json', b: 'file2.yml', style: 'plain', expected: 'result_plain.txt'
+    a: 'file1.json', b: 'file3.yaml', style: 'stylish', expected: 'result_with_empty_file.txt',
   },
   {
-    a: 'file1.json', b: 'file2.yml', style: 'json', expected: 'result_JSON.txt'
+    a: 'file1.json', b: 'file2.yml', style: 'plain', expected: 'result_plain.txt',
+  },
+  {
+    a: 'file1.json', b: 'file2.yml', style: 'json', expected: 'result_JSON.txt',
   },
 ])('test', (
-  { a, b, style, expected }
-  ) => {
+  {
+    a, b, style, expected,
+  }
+) => {
   test('gendiff', () => {
     const file1 = getFilePath(a);
     const file2 = getFilePath(b);
